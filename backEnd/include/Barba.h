396fa3e42f601e4f4a8a19429b841c62ff7f20dd
José Guilherme <gui222013@hotmail.com>
2025-10-12 19:35:20 -0300

    feat: cria a classe barba

---
+++ backEnd/include/Barba.h
@@ -0,0 +1,33 @@
+#ifndef BARBA_H
+#define BARBA_H
+
+#include <Servico.h>
+#include <string>
+
+using namespace std;
+
+
+class Barba : public Servico
+{
+private:
+    float preco;
+    string descricao;
+
+public:
+    // Construtor
+    Barba(float preco = 15.0, const string& descricao = "Barba Completa");
+
+    // MÉTODOS SOBREESCRITOS (polimorfismo)
+    void exibir() const override;           // mostra informações da barba
+    float calcularPreco() const override;   // retorna o preço
+
+    // MÉTODOS DE ACESSO E MODIFICAÇÃO
+    float getPreco() const;
+    string getDescricao() const;
+    void setPreco(float novoPreco);
+    void setDescricao(const string& novaDescricao);
+
+}
+
+
+#endif
