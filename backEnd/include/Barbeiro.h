e17c810fce85d366e0d63259a83defa4939d9f79
Alisson da Silva Bernardino <asb3@cin.ufpe.br>
2025-10-21 23:22:45 -0300

    falta unir com agendamento

--- backEnd/include/Barbeiro.h
+++ backEnd/include/Barbeiro.h
@@ -4,17 +4,11 @@
 #include <iostream>
 
 using namespace std;
-struct agendamento
-{
-    string nome;
-    int horario;
-};
 
 
 class Barbeiro: public Funcionarios{
     private:
-        vector <agendamento> agendamentos;
+        //agendamentos
     public:
-        bool agendar(string nome, int horario);
         bool estaDisponivel(int horario);
 };
--- backEnd/src/Barbeiro.cpp
+++ backEnd/src/Barbeiro.cpp
@@ -2,20 +2,6 @@
 #include <iostream>
 using namespace std;
 
-
-bool Barbeiro::agendar(string nome, int horario){
-    agendamento temp;
-    if(horario <= getHorarioTrabalhoFinal() && horario >= getHorarioTrabalhoInicial()){
-        if(estaDisponivel(horario)){
-            temp.nome = nome;
-            temp.horario = horario;
-            agendamentos.push_back(temp);
-        }
-    }else{
-        return false;
-    }
-}
-
 bool Barbeiro::estaDisponivel(int horario){
     for(int i=0;i<agendamentos.size();i++){
         if(horario == agendamentos[i].horario){
