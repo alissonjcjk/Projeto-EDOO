ee7ada1617dc6dc22ce38362f64cde61fcadd7c7
José Guilherme <gui222013@hotmail.com>
2025-10-31 17:25:35 -0300

    feat: cria pagina de clientes

---
+++ frontEnd/src/pages/Clientes.tsx
@@ -0,0 +1,8 @@
+export default function ClientesPage() {
+	return (
+		<div className="p-6">
+			<h1 className="text-3xl font-semibold mb-4">Clientes</h1>
+			<p className="text-xl text-gray-600">Gerencie a base de clientes da barbearia</p>
+		</div>
+	)
+}
