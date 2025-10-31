b5dfae3427096e01bf99c6a289de433dec2eb378
José Guilherme <gui222013@hotmail.com>
2025-10-31 17:25:53 -0300

    feat: cria pagina de funcionarios

---
+++ frontEnd/src/pages/Funcionarios.tsx
@@ -0,0 +1,8 @@
+export default function FuncionariosPage() {
+	return (
+		<div className="p-6">
+			<h1 className="text-3xl font-semibold mb-4">Funcionários</h1>
+			<p className="text-xl text-gray-600">Gerencie a equipe da barbearia</p>
+		</div>
+	)
+}
