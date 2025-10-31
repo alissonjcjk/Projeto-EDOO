f1de9d453eb27259bc3427835c112a12a2c32b55
José Guilherme <gui222013@hotmail.com>
2025-10-31 17:25:23 -0300

    feat: cria pagina de agendamentos

---
+++ frontEnd/src/pages/Agendamentos.tsx
@@ -0,0 +1,8 @@
+export default function AgendamentosPage() {
+	return (
+		<div className="p-6">
+			<h1 className="text-3xl font-semibold mb-4">Agendamentos</h1>
+			<p className="text-xl text-gray-600">Gerencie todos os agendamentos da barbearia</p>
+		</div>
+	)
+}
